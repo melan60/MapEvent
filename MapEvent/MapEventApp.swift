59b25ee4f21324c6cbd48aa79307211172aa7d87
melan60 <melan60@free.fr>
2023-11-29 16:25:24 +0100

    Initial Commit

---
+++ MapEvent/MapEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapEventApp.swift
+//  MapEvent
+//
+//  Created by benoit melanie on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
