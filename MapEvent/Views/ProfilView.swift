9ed08a9f2aa7b949542b1787cbbe963d5b49bf20
melan60 <melan60@free.fr>
2023-12-06 15:39:51 +0100

    added Profil View

---
+++ MapEvent/Views/ProfilView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfilView.swift
+//  MapEvent
+//
+//  Created by benoit melanie on 04/12/2023.
+//
+
+import SwiftUI
+
+struct ProfilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfilView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilView()
+    }
+}
