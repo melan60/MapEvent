cbf94d46c6ab1d77c29b48daa199dba49abbd161
Adrien <adrien.fernandes@edu.univ-fcomte.fr>
2023-12-06 17:02:15 +0100

    MapView

---
+++ MapEvent/Views/MapView.swift
@@ -0,0 +1,39 @@
+//
+//  MapView.swift
+//  MapEvent
+//
+//  Created by fernandes adrien on 06/12/2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
+    var body: some View {
+        Map(coordinateRegion: $region, annotationItems: locations) { location in
+            MapMarker(coordinate: location.coordinate)
+        }
+        .navigationTitle("Map")
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
